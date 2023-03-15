7e37bf7acfca8ea95bca70030f1afe78860909f2
LoganRiddle <121885823+LoganRiddle@users.noreply.github.com>
2023-03-15 13:25:51 -0400

    Cleanup

--- vulcan-editor/src/edit.rs
+++
@@ -1,5 +0,0 @@
-fn replace_ascii_char_at(s: String, idx: uint, c: char) -> String {
-    let mut ascii_s = s.into_ascii();
-    ascii_s[idx] = c.to_ascii();
-    String::from_utf8(ascii_s.into_bytes()).unwrap()
-}
